01f0476d5246222afab9d1f89e07e047ee20fd17
Helen Gardner <hellsgard@gmail.com>
2022-01-07 14:15:43 +0000

    added index stuff

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const lizardRoutes = require('./routes/lizardRoutes.js');
+
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect('mongodb+srv://root:root@cluster0.vtwju.mongodb.net/beers_database/beers', {useNewUrlParser: true}, 
+(error) => {
+    if(error) {
+        console.log("Cant connect to DB :(");
+    } else if (!error) {
+        console.log("connected to DB :)");
+    }
+});
+
+app.use('/beer', beerRoutes);
+
