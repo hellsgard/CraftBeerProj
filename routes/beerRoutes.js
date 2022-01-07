fe93077bd68c7e2744f6dfe562c49690f9dd3ec1
JennyE7 <jenniferjayne.me@hotmail.com>
2022-01-07 14:56:37 +0000

    added updateById

--- routes/beerRoutes.js
+++ routes/beerRoutes.js
@@ -3,7 +3,18 @@ const router = require('express').Router();
 const {Beer} = require('../persistence/models/beer');
 
 router.get('/get/:id', (req, res) => {
-    Beer.findById(req.params.id, (error, lizard) => {
-        res.status(200).send(lizard);
+    Beer.findById(req.params.id, (error, beer) => {
+        res.status(200).send(beer);
     })
-})
+});
+
+router.get('/update/:id', (req, res) => {
+    Beer.findByIdAndUpdate(req.params.id, req.body, (error, beer) => {
+        if(error) {
+            console.log(error);
+        }
+        else {
+            res.status(202).send(`updated ${beer}.`);
+        }
+    })
+});
